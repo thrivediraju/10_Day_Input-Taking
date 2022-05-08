b8ec5e5d405c8b89e5ca429b10c1553c62712221
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-08 14:07:37 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,39 @@
+# a=5; b=6  # providing the values statically
+# print(a+b)
+
+# x=input()
+# y=input()
+# print(x+y)
+# print(int(x)+int(y))
+
+# a=input('Enter your Name:')
+# print('My Name is',a)
+
+# b=int(input())
+# print(b,type(b))
+
+# c=int(input('Enter your Phone Number:'))
+# print('Your Contact No. is:',c)
+
+# d=float(input())
+# print(d,type(d))
+
+# e=float(input('Enter your Weight:'))
+# print(e,type(e))
+
+'''
+Homework
+1) print your Name
+2) School Name  / College Name
+3) Your RollNumber
+4) your percentage
+5) your height
+6) Fav Movie
+
+all the inputs should be taken by the user.
+
+Ex:
+
+My Name is XYZ , School Name is ABC , RollNo: 13MRU 
+Percentage is 88.83 and fav movie is PQR
+'''    
